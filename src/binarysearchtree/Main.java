1a91ef0d514458a80414fa7f631faf6f248fbf71
Mark <macbhunu@gmail.com>
2025-03-10 14:32:10 +0200

    Added the bst and stack with two queues

---
+++ src/binarysearchtree/Main.java
@@ -0,0 +1,7 @@
+package binarysearchtree;
+
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+    }
+}
---
+++ src/queues/TwoStacks/MyQueue.java
@@ -0,0 +1,46 @@
+package queues.TwoStacks;
+
+import java.util.Stack;
+
+public class MyQueue {
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+
+    public MyQueue() {
+    }
+
+    public void enqueue(int value) {
+
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+
+        stack1.push(value);
+
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+
+    }
+
+    public void printAll(){
+        for (int i = stack1.size() -1; i >= 0; i--) {
+            System.out.println(stack1.get(i));
+        }
+    }
+
+    public void dequeue(){
+        System.out.println(stack1.pop());
+    }
+
+    public void pop(){
+        System.out.println(stack1.pop());
+    }
+
+     public void peek(){
+        System.out.println(stack1.pop());
+        System.out.println(stack1.pop());
+        System.out.println(stack1.pop());   
+    
+    }
+}
