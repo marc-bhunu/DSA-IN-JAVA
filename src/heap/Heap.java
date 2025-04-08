4678e2f9a1bda3c7959b8e81abd40da90e97c5eb
Mark <macbhunu@gmail.com>
2025-04-08 07:11:09 +0200

    Made changes to the heap

--- src/heap/Heap.java
+++ src/heap/Heap.java
@@ -1,13 +1,23 @@
 package heap;
 
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Heap {
 
     /*
-     * the right child is always one more to the left but since, we are zero based
-     * it will be + 2
-     * same goes for everything since its not zero based
+     * Math for the heaps non 0 based indexing
+     * leftChild = 2 * parentIndex
+     * rightChild = 2 * parentIndex + 1
+     * parrentNode = (integer division of an index by 2) NB remember that for
+     * integer division we have to drop the decimal part
+     */
+
+    /*
+     * Math for the heaps 0 based indexing
+     * leftChild = 2 * parentIndex + 1
+     * rightChild = 2 * parentIndex + 2
+     * parrentNode = integer division -> (index -1) /2
      */
 
     private List<Integer> heap;
@@ -17,18 +27,17 @@ public class Heap {
     }
 
     public List<Integer> getHeap() {
-        return new ArrayList<>();
+        return new ArrayList<>(heap);
     }
 
     public int leftChild(int index) {
-        return 2 * (index + 1);
+        return 2 * index + 1;
     }
 
     public int rightChild(int index) {
-        return 2 * (index + 2);
+        return 2 * index + 2;
     }
 
- 
     private int parent(int index) {
         return (index - 1) / 2;
     }
@@ -37,18 +46,50 @@ public class Heap {
         int temp = heap.get(index1);
         heap.set(index1, heap.get(index2));
         heap.set(index2, temp);
-
     }
 
-    public void insert(int value){
+    public void insert(int value) {
         heap.add(value);
         int current = heap.size() - 1;
-
-        while (current > 0 && heap.get(current) >heap.get(parent(current))) {
+        while (current > 0 && heap.get(current) > heap.get(parent(current))) {
             swap(current, parent(current));
             current = parent(current);
-            
         }
+    }
+
+    public void sinkDown(int index) {
+        int maxIndex = index;
+        while (true) {
+            int leftIndex = leftChild(index);
+            int rightIndex = rightChild(index);
+            if (leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)) {
+                maxIndex = leftIndex;
+            }
+            if (rightIndex < heap.size() && heap.get(rightIndex) > heap.get(maxIndex)) {
+                maxIndex = rightIndex;
+            }
+            if (maxIndex != index) {
+                swap(index, maxIndex);
+                index = maxIndex;
+            } else {
+                return;
+            }
+        }
+    }
+
+    public Integer remove() {
+        if (heap.size() == 0) {
+            return null;
+        }
+        if (heap.size() == 1) {
+            return heap.remove(0);
+        }
+        int maxValue = heap.get(0);
+
+        heap.set(0, heap.remove(heap.size() - 1));
+        sinkDown(0);
+
+        return maxValue;
     }
 
 }
