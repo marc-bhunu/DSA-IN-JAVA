a3ff2d2d44a7ca4102946827931b81f0943edde6
Mark <macbhunu@gmail.com>
2025-11-09 19:11:58 +0200

    Add powerN method for recursive exponentiation and update main method

--- src/recursion_kimi/codingbat/PowerN.java
+++ src/recursion_kimi/codingbat/PowerN.java
@@ -1,4 +1,13 @@
 package recursion_kimi.codingbat;
 
 public class PowerN {
+
+    public static int powerN(int base, int n){
+        if (n == 1) return  base;
+        return base * powerN(base, n -1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(powerN(4, 3));
+    }
 }
