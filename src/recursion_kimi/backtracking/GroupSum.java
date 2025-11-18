76385db620fcd1df6eb9e701e38cb71b5325fa0c
Mark <macbhunu@gmail.com>
2025-11-18 14:21:45 +0200

    Added Groupsum problem solution

---
+++ src/recursion_kimi/backtracking/GroupSum.java
@@ -0,0 +1,16 @@
+package recursion_kimi.backtracking;
+
+public class GroupSum {
+
+    public static boolean groupSum(int start, int[] arr, int target){
+        if (start >= arr.length) return target == 0;
+        if (groupSum(start + 1, arr, target - arr[start])) return true;
+        if (groupSum(start + 1, arr, target)) return  true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int [] arr ={2, 4, 8};
+        System.out.println(groupSum(0, arr, 10));
+    }
+}
--- src/recursion_kimi/backtracking/Subsets.java
+++ src/recursion_kimi/backtracking/Subsets.java
@@ -1,24 +1,29 @@
-package recursion_kimi.backtracking.generatesubsets;
+package recursion_kimi.backtracking;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class Subsets {
 
-    public List<List<Integer>> subsets(int[] nums) {
+    public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> current = new ArrayList<>();
         subset(nums, current, result, 0);
         return result;
     }
 
-    void subset(int[] nums, List<Integer> current, List<List<Integer>> result, int index) {
+    static void  subset(int[] nums, List<Integer> current, List<List<Integer>> result, int index) {
         result.add(new ArrayList<>(current));
         for (int i = index; i < nums.length; i++) {
             current.add(nums[i]);
             subset(nums,  current, result, i+1);
             current.remove(current.size() - 1);
         }
+    }
+
+    public static void main(String[] args) {
+        int[] arr ={2, 4, 8};
+        System.out.println(subsets(arr));
     }
 
 
