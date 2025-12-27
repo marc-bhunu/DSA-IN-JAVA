da6d98c188d06a06937167080fdf18b70b9c67b8
Mark <macbhunu@gmail.com>
2025-12-27 10:35:36 +0200

    Added new module

--- dsa-code/src/leetcode/validparanthesis/Main.java
+++ dsa-code/src/leetcode/validparanthesis/Main.java
@@ -1,3 +1,5 @@
+package leetcode.validparanthesis;
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
--- dsa-code/src/leetcode/validparanthesis/Solution.java
+++ dsa-code/src/leetcode/validparanthesis/Solution.java
@@ -1,3 +1,5 @@
+package leetcode.validparanthesis;
+
 import java.util.Stack;
 
 public class Solution {
